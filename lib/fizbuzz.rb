4013710740087ef3618a865f7c58631ff39a7bb2
Neto <fpottinger.marques@gmail.com>
2021-12-01 10:30:11 +0000

    added working code to fizbuzz.rb

---
+++ lib/fizbuzz.rb
@@ -0,0 +1,15 @@
+def fizbuzz(number)
+    
+  if number % 3 == 0 && number % 5 == 0
+    return "fizzbuzz"
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else
+    number
+  end 
+
+end
+
+p fizbuzz(3)
